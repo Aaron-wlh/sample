011dd2ee34dd9a112a93cddb171e1d5c7bfc9249
Aaron_wlh <976724250@qq.com>
2017-12-09 05:06:42 +0000

    xiugai

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $users = factory(User::class)->times(50)->make();
+        // User::insert($users->makeVisible(['password', 'remember_token'])->toArray());
+        factory(User::class, 20)->create();
+        $user = User::find(1);
+        $user->name = 'wlh';
+        $user->email = '111@qq.com';
+        $user->password = 111;
+        $user->is_admin = true;
+        $user->save();
+    }
+}
