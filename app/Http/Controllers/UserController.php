92afe1f81ed470e689c8dd5e9452b445b625d285
Aaron_wlh <976724250@qq.com>
2017-12-09 04:59:50 +0000

    Finish user edit, update, index, and destroy actions

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,22 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth', [
+            'except' => ['show', 'create', 'store']
+        ]);
+        $this->middleware('guest', [
+            'only' => ['create']
+        ]);
+    }
+
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
+
     public function create()
     {
         return view('users.create');
@@ -18,16 +34,51 @@ class UserController extends Controller
         return view('users.show', compact('user'));
     }
 
+    //用户注册
     public function store(Request $request)
     {
         $this->validate($request, [
             'name' => 'required|max:50',
             'email' => 'required|email|unique:users|max:255',
-            'password' => 'required|confirmed'
+            'password' => 'required|confirmed|min:6'
         ]);
         $user = User::create($request->all());
         Auth::login($user);
         //session()->flash('success', '欢迎，您将在这里开启一段新的旅程~');
         return redirect('/users/' . $user->id)->with(['success' => '欢迎，您将在这里开启一段新的旅程~']);
+    }
+
+    public function edit(User $user)
+    {
+        $this->authorize('update', $user);
+        return view('users.edit', compact('user'));
+    }
+
+    //用户编辑
+    public function update(Request $request, $id)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:50',
+            'password' => 'nullable|confirmed|min:6'
+        ]);
+        $data = [];
+        $data['name'] = $request->name;
+        if ($request->password) {
+            $data['password'] = $request->password;
+        }
+        $user = User::find($id);
+        $this->authorize('update', $user);
+        $user->update($data);
+        session()->flash('success', '修改信息成功~');
+        return redirect()->route('users.show', $user->id);
+    }
+
+    //删除用户
+    public function destroy(User $user)
+    {
+        $this->authorize('destroy', $user);
+        $user->delete();
+        session()->flash('success', '成功删除用户！');
+        return back();
     }
 }
